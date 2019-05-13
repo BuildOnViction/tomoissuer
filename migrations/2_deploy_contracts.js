064599418e47c167839483b31b38bfc126e5fa37
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2019-05-13 09:21:46 +0000

    add trc21

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,13 @@
-const Issuer = artifacts.require('./Issuer');
-const BigNumber = require('bignumber.js');
+const Issuer = artifacts.require('./Issuer')
+const MyTRC21 = artifacts.require('./MyTRC21')
+const BigNumber = require('bignumber.js')
 
 const config = require('config');
 
 module.exports = function(deployer) {
-    return deployer.deploy(Issuer, 900, (new BigNumber(1).multipliedBy(1e+18)).toString(10));
+    return deployer.deploy(Issuer, 900, (new BigNumber(1).multipliedBy(1e+18)).toString(10)).then(() => {
+        return deployer.deploy(MyTRC21, 'MyTRC21',
+            'MyTRC21', 18, (new BigNumber(10000000).multipliedBy(1e+18)).toString(10),
+            (new BigNumber(1).multipliedBy(1e+18)).toString(10))
+    })
 };
