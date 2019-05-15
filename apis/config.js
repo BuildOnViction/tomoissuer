4fe6c9d2b3f7efb64badb05eb4858a5eaf63810c
Vu Pham <pqv199x@hotmail.com>
2019-05-15 16:04:32 +0700

    trezor loging in

--- apis/config.js
+++ apis/config.js
@@ -1,9 +1,11 @@
 'use strict'
 const express = require('express')
+const config = require('config')
 const router = express.Router()
 
 router.get('/', async function (req, res, next) {
     let appConfig = {}
+    appConfig.blockchain = config.get('blockchain')
     return res.json(appConfig)
 })
 
