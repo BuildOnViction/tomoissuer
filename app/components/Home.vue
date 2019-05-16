b9caa0a7eee227d667f0b2c80b4642d136f96a43
Vu Pham <pqv199x@hotmail.com>
2019-05-16 15:21:26 +0700

    Ledger loging in mechanism

---
+++ app/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+    <div
+        v-if="account"
+        style="text-align: center; padding-top: 10em">
+        Welcome {{ account }}
+    </div>
+</template>
+
+<script>
+import store from 'store'
+
+export default {
+    name: 'App',
+    components: {},
+    mixins: [],
+    data () {
+        return {
+            account: ''
+        }
+    },
+    validations: {},
+    computed: {},
+    watch: {},
+    updated () {},
+    beforeDestroy () {},
+    created: async function () {
+        this.account = store.get('address')
+    },
+    mounted () {},
+    methods: {}
+}
+</script>
