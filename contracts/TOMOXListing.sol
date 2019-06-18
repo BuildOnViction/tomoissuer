c3bca7b030576bba20d6e9932da9bbbf5d8b182e
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2019-06-18 09:25:25 +0000

    update TomoX Listing contract

--- contracts/TOMOXListing.sol
+++ contracts/TOMOXListing.sol
@@ -2,29 +2,20 @@ pragma solidity ^0.4.24;
 
 contract TOMOXListing {
 
-    uint256 _minApply;
     address[] _tokens;
     mapping(address => TokenState) tokensState;
 
 
     struct TokenState {
-        uint256 capacity;
         bool isActive;
     }
 
-    constructor (uint256 value) public {
-        _minApply = value;
-    }
+    constructor () public { }
 
     modifier onlyValidApplyNewToken(address token){
         require(token != address(0));
-        require(msg.value >= _minApply);
         require(tokensState[token].isActive != true);
         _;
-    }
-
-    function minApply() public view returns(uint256) {
-        return _minApply;
     }
 
     function tokens() public view returns(address[]) {
@@ -35,11 +26,10 @@ contract TOMOXListing {
         return tokensState[token].isActive;
     }
 
-    function applyToken(address token) public payable onlyValidApplyNewToken(token){
+    function apply(address token) public payable onlyValidApplyNewToken(token){
         _tokens.push(token);
         tokensState[token] = TokenState({
-            isActive: true,
-            capacity: msg.value
+            isActive: true
         });
     }
 }
