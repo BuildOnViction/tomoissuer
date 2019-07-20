f5be9a1d481733b5306416943c0c0c78b1fbb988
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2019-07-20 04:30:53 +0000

    add Mintable token

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,7 @@
 const TRC21Issuer = artifacts.require('./TRC21Issuer')
 const TOMOXListing = artifacts.require('./TOMOXListing')
 const MyTRC21 = artifacts.require('./MyTRC21')
+const MyTRC21Mintable = artifacts.require('./MyTRC21Mintable')
 const BigNumber = require('bignumber.js')
 
 const config = require('config');
@@ -11,7 +12,11 @@ module.exports = function(deployer) {
             return deployer.deploy(MyTRC21, 'MyTRC21',
                 'MyTRC21', 18, (new BigNumber(10000000).multipliedBy(1e+18)).toString(10),
                 (new BigNumber(1).multipliedBy(1e+18)).toString(10)).then(() => {
-                    return true
+                    return deployer.deploy(MyTRC21Mintable, 'MyTRC21Mintable',
+                        'MyTRC21Mintable', 18, (new BigNumber(10000000).multipliedBy(1e+18)).toString(10),
+                        (new BigNumber(1).multipliedBy(1e+18)).toString(10)).then(() => {
+                            return true
+                        })
                 })
         })
     })
