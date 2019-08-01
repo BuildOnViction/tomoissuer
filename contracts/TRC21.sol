dc029db4af12c15f521a7341a8e69327fbf81b63
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2019-08-01 08:43:40 +0000

    fix indent

--- contracts/TRC21.sol
+++ contracts/TRC21.sol
@@ -64,27 +64,27 @@ library SafeMath {
  * @title TRC21 interface
  */
 interface ITRC21 {
-	function totalSupply() external view returns (uint256);
+    function totalSupply() external view returns (uint256);
 
-	function balanceOf(address who) external view returns (uint256);
+    function balanceOf(address who) external view returns (uint256);
 
     function issuer() external view returns (address);
 
-	function estimateFee(uint256 value) external view returns (uint256);
+    function estimateFee(uint256 value) external view returns (uint256);
 
-	function allowance(address owner, address spender) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
 
-	function transfer(address to, uint256 value) external returns (bool);
+    function transfer(address to, uint256 value) external returns (bool);
 
-	function approve(address spender, uint256 value) external returns (bool);
+    function approve(address spender, uint256 value) external returns (bool);
 
-	function transferFrom(address from, address to, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
 
-	event Transfer(address indexed from, address indexed to, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
-	event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
-	event Fee(address indexed from, address indexed to, address indexed issuer, uint256 value);
+    event Fee(address indexed from, address indexed to, address indexed issuer, uint256 value);
 }
 
 /**
