5082129275c645c3cb47c9cbb6ccc66b4ad26c4b
Bob Jr <nguyennt@coin98.finance>
2024-01-11 09:58:23 +0700

    Update networkId check

--- app/walletAdapter.js
+++ app/walletAdapter.js
@@ -32,7 +32,7 @@ export default {
                 method: 'net_version'
             })
             try {
-                if (parseInt(supportedWalletOption[0].chainId, 10) !== parseInt(chainId, 10)) {
+                if (parseInt(supportedWalletOption[0].chainId) !== parseInt(chainId)) {
                     await provider.request({
                         method: 'wallet_addEthereumChain',
                         params: supportedWalletOption
@@ -87,7 +87,7 @@ export default {
                 method: 'net_version'
             })
             try {
-                if (parseInt(supportedWalletOption[0].chainId, 10) !== parseInt(chainId, 10)) {
+                if (parseInt(supportedWalletOption[0].chainId) !== parseInt(chainId)) {
                     await provider.request({
                         method: 'wallet_addEthereumChain',
                         params: supportedWalletOption
@@ -122,7 +122,7 @@ export default {
                 method: 'net_version'
             })
             try {
-                if (parseInt(supportedWalletOption[0].chainId, 10) !== parseInt(chainId, 10)) {
+                if (parseInt(supportedWalletOption[0].chainId) !== parseInt(chainId)) {
                     await provider.request({
                         method: 'wallet_addEthereumChain',
                         params: supportedWalletOption
